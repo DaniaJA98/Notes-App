2fb345cec560d54367fc09308da63409766839ca
Dania Jimenez <alexandrajimenes64@gmail.com>
2022-06-08 16:11:40 -0300

    fix-error

--- api/src/index.js
+++ api/src/index.js
@@ -3,7 +3,9 @@ require('dotenv').config();
 const app = require('./app');
 require('./database');
 
-function main() { app.listen(app.get('port'));
+
+async function main() {
+    await app.listen(app.get('port'));
     console.log('server on port' , app.get('port'));
 }
 
