a6a9f67b4a2145f5c32b4e666dd9884bb37c2693
Dania Jimenez <alexandrajimenes64@gmail.com>
2022-06-08 16:08:47 -0300

    ..

--- api/src/index.js
+++ api/src/index.js
@@ -3,9 +3,7 @@ require('dotenv').config();
 const app = require('./app');
 require('./database');
 
-// Funcion principal que inicia el programa
-async function main() {
-    await app.listen(app.get('port'));
+async function main() { app.listen(app.get('port'));
     console.log('server on port' , app.get('port'));
 }
 
